90aaabbdd03620dfbd384defc4579828655cc5d6
dhruv <dhruvj797@gmail.com>
2024-07-24 10:00:21 +0530

    edited build workflow
    Signed-off-by: dhruv <dhruvj797@gmail.com>

--- helloworld/src/main/java/App.java
+++ helloworld/src/main/java/App.java
@@ -2,6 +2,6 @@ package dev.jaydee029.helloworld;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World 4!");
+        System.out.println("Hello, World 5!");
     }
 }
